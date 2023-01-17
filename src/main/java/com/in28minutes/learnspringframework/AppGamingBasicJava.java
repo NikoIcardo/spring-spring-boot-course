619d4259cfe9ddc62c02e3c12a66e10e0c38dd60
nikoicardo <nikoicardo@gmail.com>
2023-01-17 08:38:49 -0800

    Add Gaming Console Interface and update Games to implement it and the game runner to use GamingConsole Objects rather than the individual tightly coupled classes.

--- src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -2,14 +2,17 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacManGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 
-//		var marioGame = new MarioGame(); 
-		var superContraGame = new SuperContraGame(); 
-		var gameRunner = new GameRunner(superContraGame); 
+        var game = new MarioGame(); 
+		var game1 = new SuperContraGame(); 
+        var game2 = new PacManGame(); 
+		var gameRunner = new GameRunner(game2); 
 		gameRunner.run(); 
 	}
 
