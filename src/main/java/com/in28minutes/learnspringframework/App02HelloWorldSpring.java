ecd35ac0f3e0b65e98a61a294301c86ef8815f01
nikoicardo <nikoicardo@gmail.com>
2023-01-18 12:23:33 -0800

    Explore different ways to create and call beans

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -15,9 +15,25 @@ public class App02HelloWorldSpring {
 		 * 1. Launch Spring Context -
 		 * 2. Configure the things we want spring to manage - @Configuration
 		 */ 
+		
+		
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class); 
+		
 		System.out.println(context.getBean("name"));
 		
+		System.out.println(context.getBean("age")); 
+		
+		System.out.println(context.getBean("person")); 
+		
+		System.out.println(context.getBean("person2MethodCall"));
+		
+		// note that to call the bean, we don't need to actually pass the parameters
+		System.out.println(context.getBean("person3Parameters"));
+		
+		System.out.println(context.getBean("address2"));
+		
+		System.out.println(context.getBean(Address.class));
 	}
 
 }
+ 
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,11 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+// Records store values
+record Person(String name, int age, Address address) {}; 
+record Address(String firstLine, String city) {}; 
+
 // where you define beans and your spring configuration
 @Configuration
 public class HelloWorldConfiguration {
@@ -13,5 +18,37 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Niko"; 
 	}
-
+	
+	@Bean
+	public int age() {
+		return 25; 
+	}
+	
+    @Bean 
+    public Person person() {
+    	return new Person(name(), age(), new Address("101 Main St.", "Seattle")); 
+    }
+    
+    // The following person methods are important to note because you can use existing beans to create new beans 
+    @Bean
+    public Person person2MethodCall() {
+    	return new Person(name(), age(), address()); 
+    }
+    
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+    	// parameters using beans need to use the assigned name of the bean
+    	// note that these are auto wired. 
+    	return new Person(name, age, address3); 
+    }
+    
+    @Bean(name = "address2")
+    public Address address() {
+    	return new Address("101 Main St.", "Seattle");
+    }
+    @Bean(name = "address3")
+    public Address address3() {
+    	return new Address("101 Main St.", "Seattle");
+    }
+     
 }
