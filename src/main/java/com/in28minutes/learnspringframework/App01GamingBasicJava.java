be018944716ac039f56d58190f554dae1edac1fd
nikoicardo <nikoicardo@gmail.com>
2023-01-18 16:37:34 -0800

    Implement a spring IOC container in the previous game console code converting both the game runner and game console classes into spring beans.

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -1,5 +1,10 @@
 package com.in28minutes.learnspringframework;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacManGame;
@@ -9,10 +14,13 @@ public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 
-        var game = new MarioGame(); 
-		var game1 = new SuperContraGame(); 
-        var game2 = new PacManGame(); 
-		var gameRunner = new GameRunner(game2); 
-		gameRunner.run(); 
+       try (var context = new AnnotationConfigApplicationContext(GamingBasicConfiguration.class)){
+    	   System.out.println(context.getBean("gameRunner"));
+    	   
+    	   Consumer<String> printToConsole = str -> System.out.println(str); 
+    	   
+    	   Arrays.stream(context.getBeanDefinitionNames()).forEach(printToConsole); 
+       }
 	}
+
 }
